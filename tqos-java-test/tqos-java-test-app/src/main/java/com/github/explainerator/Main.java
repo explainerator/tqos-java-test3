14a457f7085de3fde0de462256440957c641b248
unknown <kduck@twoducks.ca>
2021-12-21 21:55:56 -0400

    Add logging

--- tqos-java-test/tqos-java-test-app/src/main/java/com/github/explainerator/Main.java
+++ tqos-java-test/tqos-java-test-app/src/main/java/com/github/explainerator/Main.java
@@ -1,9 +1,13 @@
 package com.github.explainerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
 
 public class Main {
+    protected static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
 
-        System.err.println("Hello Error");
+        logger.error("Hello Error");
     }
 }
